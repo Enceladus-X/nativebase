c870a9046f7c997bb97ee41c3f5929948aed23f9
Enceladus-X <106950964+Enceladus-X@users.noreply.github.com>
2024-04-29 16:38:15 +0900

    파이어베이스 코드 삭제
    좀있다가 한번에 하는걸로

--- capstion/App.js
+++ capstion/App.js
@@ -4,29 +4,6 @@ import { NavigationContainer } from "@react-navigation/native";
 import { NativeBaseProvider } from "native-base";
 
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAnVlynd8sfH4KzW897ZktB-ahe5jfCu3M",
-  authDomain: "capstone-d59c2.firebaseapp.com",
-  databaseURL: "https://capstone-d59c2-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "capstone-d59c2",
-  storageBucket: "capstone-d59c2.appspot.com",
-  messagingSenderId: "517618805917",
-  appId: "1:517618805917:web:d5a3961d7aaab9d1427874",
-  measurementId: "G-DFN95B0W1M"
-};
-
-
-// Initialize Firebase
-initializeApp(firebaseConfig);
-
 export default function App() {
   return (
     <NativeBaseProvider>
